4f54b1a6af8cb6403c166add7fcaa377dccc95f3
jasonchang1128 <43047434+jasonchang1128@users.noreply.github.com>
2019-12-05 00:18:15 -0500

    Added Part 2 for Secure Container.
    Adds in the rule more than two adjacent don't count.

--- 4_SecureContainer.py
+++ 4_SecureContainer.py
@@ -18,3 +18,31 @@ for puzzle_num in range(MinPuzzleInput, MaxPuzzleInput):
         num_possible += 1
 
 print( "The number of possible passwords are: " + str(num_possible))
+
+#  Part Two
+num_possible = 0
+for puzzle_num in range(MinPuzzleInput, MaxPuzzleInput):
+    digit_list = [int(digit) for digit in str(puzzle_num)]
+    is_possible_num = 0
+    for idx in range(1,6): # six digits
+        if digit_list[idx] == digit_list[idx-1]:
+            if idx == 1:
+                if digit_list[idx + 1] != digit_list[idx]:
+                    is_possible_num = 1  # at least one adjacent matching digits
+                    break
+            elif idx == 5:
+                if digit_list[idx-2] != digit_list[idx]:
+                    is_possible_num = 1  # at least one adjacent matching digits
+                    break
+            elif digit_list[idx-2] != digit_list[idx] and digit_list[idx+1] != digit_list[idx]:
+                is_possible_num = 1  # at least one adjacent matching digits
+                break
+
+    for idx in range(1,6): # six digits
+        if digit_list[idx] < digit_list[idx-1]:  # Going from left to right, the digits never decrease;
+            is_possible_num = 0
+            break
+    if is_possible_num == 1:
+        num_possible += 1
+
+print( "The number of possible passwords are: " + str(num_possible))
