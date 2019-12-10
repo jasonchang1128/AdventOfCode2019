da002e04bb5a85f91a0c6b47a09e419b8dc312ed
jasonchang1128 <43047434+jasonchang1128@users.noreply.github.com>
2019-12-09 21:21:36 -0500

    Day 7 Part 1
    Really ugly looking code for the phase sequences.
    
    Added ability to have multiple inputs.

---
+++ 7_AmpCircuit.py
@@ -0,0 +1,103 @@
+# Day 7 of Advent of Code.
+# Part 1 - Running IO to amp circuit.
+
+def opcode2_mult(parlist, pos1, pos2, outpos):
+    parlist[outpos] = parlist[pos1] * parlist[pos2]
+
+def val_from_parameter_mode(parameter_mode, desired_list, param_position):
+    if parameter_mode == 1:  # Immediate Mode.
+        return desired_list[param_position]
+    elif parameter_mode == 0:  # Position Mode.
+        return desired_list[desired_list[param_position]]
+
+def get_diag_output(instruction_list, *inputs):
+    input_num = 0
+    CurPos = 0
+    while instruction_list[CurPos] != 99:
+        cur_opcode_padded = str(instruction_list[CurPos]).zfill(5)
+        opcode_list = [int(digit) for digit in cur_opcode_padded]  # Turn the opcode into a list of digits.
+        actual_opcode = int(str(opcode_list[3]) + str(opcode_list[4]))  #Concat the actual opcode digits.
+        if actual_opcode == 1:  # Add Opcode, read, read, Write.
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            instruction_list[instruction_list[CurPos + 3]] = param1 + param2  # always position mode.
+            CurPos += 4
+        elif actual_opcode == 2:  # Mult opcode, read, read, write.
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            instruction_list[instruction_list[CurPos + 3]] = param1 * param2  # always position mode.
+            CurPos += 4
+        elif actual_opcode == 3:  # Set param pos to input.
+            instruction_list[instruction_list[CurPos+1]] = inputs[input_num]  # always position mode.
+            input_num += 1
+            CurPos += 2
+        elif actual_opcode == 4:
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            output = param1
+            CurPos += 2
+        elif actual_opcode == 5:  # jump if true: opcode, non-zero, jump-pos
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            if param1 != 0:
+                CurPos = param2
+            else:
+                CurPos += 3
+        elif actual_opcode == 6:  # jump if false: opcode, zero, jump-pos
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            if param1 == 0:
+                CurPos = param2
+            else:
+                CurPos += 3
+        elif actual_opcode == 7:  # less than: opcode, param1 < param2, 1 or 0
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            if param1 < param2:
+                instruction_list[instruction_list[CurPos + 3]] = 1
+            else:
+                instruction_list[instruction_list[CurPos + 3]] = 0
+            CurPos += 4
+        elif actual_opcode == 8:  # equals than: opcode, param1 == param2, 1 or 0
+            param1 = val_from_parameter_mode(opcode_list[2], instruction_list, CurPos+1)
+            param2 = val_from_parameter_mode(opcode_list[1], instruction_list, CurPos+2)
+            if param1 == param2:
+                instruction_list[instruction_list[CurPos + 3]] = 1
+            else:
+                instruction_list[instruction_list[CurPos + 3]] = 0
+            CurPos += 4
+        elif actual_opcode == 99:
+            print("Finished")
+            break
+        else:
+            print("Error")
+            break
+    return output
+
+
+def calc_amp_out(instruct_list, phase_setting):
+    cur_input = 0
+    for phase_num in phase_setting:
+        CurList = instruct_list.copy()
+        if phase_num == phase_setting[-1]:  # Is it the last element?
+            return get_diag_output(CurList, phase_num, cur_input)
+        else:
+            cur_input = get_diag_output(CurList, phase_num, cur_input)
+
+Instruction_Input = [3,8,1001,8,10,8,105,1,0,0,21,38,55,68,93,118,199,280,361,442,99999,3,9,1002,9,2,9,101,5,9,9,102,4,9,9,4,9,99,3,9,101,3,9,9,1002,9,3,9,1001,9,4,9,4,9,99,3,9,101,4,9,9,102,3,9,9,4,9,99,3,9,102,2,9,9,101,4,9,9,102,2,9,9,1001,9,4,9,102,4,9,9,4,9,99,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,1001,9,2,9,1002,9,4,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99]
+
+ListOfAllNumbers = []
+for i in range(43211):
+    ListOfAllNumbers.append(list(str(i).zfill(5)))
+
+AllPhasesSeq = []
+for cur_num in ListOfAllNumbers:
+    if int(cur_num[0]) < 5 and int(cur_num[1]) < 5 and int(cur_num[2]) < 5 and int(cur_num[3]) < 5 and int(cur_num[4]) < 5:
+        if len(cur_num) == len(set(cur_num)):
+            AllPhasesSeq.append([int(x) for x in cur_num])
+
+FinalOutSignals = []
+for phase_seq in AllPhasesSeq:
+    FinalOutSignals.append(calc_amp_out(Instruction_Input,phase_seq))
+
+# Day 7 Part 1 Max Output Signal.
+print(max(FinalOutSignals))
